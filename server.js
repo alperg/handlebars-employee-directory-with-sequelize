cb40c1f6603888fc1ef74c7fc653e90935ba075f
Alper Gokcehan <mail@alperg.com>
2020-09-25 10:39:05 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 
 const PORT = process.env.PORT || 4422;
+const db = require("./models");
 
 const app = express();
 
